a7ed74ccdf3af7c0835e67e1d72f23413bf41577
Tyler Townsend <townsend.ts@outlook.com>
2020-07-28 14:16:26 -0700

    Added prompt for making themes

--- src/parser.ts
+++ src/parser.ts
@@ -1,12 +1,51 @@
 const parseArgs = require('minimist');
 import * as path from 'path';
+import * as inquirer from 'inquirer';
 
 const { create, build, printUsage } = require('./lib');
 const colors = require('../templates/palette/index');
 import * as style from '../templates/style/index';
 
+interface ICliOptions {
+  themeName: string,
+  style: string,
+  palette: string,
+  outputPath: string
+}
 
-export function parse() {
+const questions = [
+{
+  name: 'name',
+  type: 'input',
+  message: 'Project name:'
+},
+{
+  name: 'style',
+  type: 'list',
+  message: 'Do you want to use the default style?',
+  choices: ['yes', 'no']
+},
+{
+  name: 'palette',
+  type: 'list',
+  message: 'Do you want to use the default palette?',
+  choices: ['yes', 'no']
+},
+{
+  name: 'customStyle',
+  type: 'input',
+  message: 'Enter in the style you wish to use',
+  when: (answers) => answers.style === 'no'
+},
+{
+  name: 'customPalette',
+  type: 'input',
+  message: 'Enter in the palette you wish to use',
+  when: (answers) => answers.palette === 'no'
+}
+];
+
+export async function parse() {
   const args = parseArgs(process.argv.slice(2), {
     alias: {
       h: 'help',
@@ -17,34 +56,45 @@ export function parse() {
     },
   });
 
-  const mode = args._
-  if (mode.length != 1) {
+  const mode = args._;
+  console.log(args);
+  if (mode.length == 0) {
+    const config = await _runPrompt();
+    console.log(`propmt with config ${JSON.stringify(config)}`)
+    _initialize({
+      ...config,
+      style: true,
+      palette: true
+    });
+    _build(config);
+  } else if (mode.length == 1) {
+    const config = _buildConfig(args);
+    console.log(config);
+    switch(mode[0]) {
+      case 'new': {
+        _initialize(config);
+        break;
+      }
+      case 'create': {
+        _build(config);
+        break;
+      }
+      default: {
+        printUsage();
+        break;
+      }
+    }
+  } else {
     printUsage();
-    return
-  }
-
-  switch(mode[0]) {
-    case 'new': {
-      _initialize(args);
-      break;
-    }
-    case 'create': {
-      _build(args);
-      break;
-    }
-    default: {
-      printUsage();
-      break;
-    }
   }
 }
 
-function _initialize(args) {
-  let style = args.style;
-  let palette = args.palette;
-  if (args.all) {
-    style = palette = true;
-  }
+function _initialize(config) {
+  const style = config.style;
+  const palette = config.palette;
+  console.log('initalizing repo');
+  console.log(style);
+  console.log(palette);
   if (palette) {
     create('palette');
   }
@@ -58,4 +108,31 @@ function _build(args) {
   const styleObj = (args.style ? require(args.style) : style);
   const appName = (args.name ? args.name : path.basename(process.cwd()));
   build(palette, styleObj, appName); 
+}
+
+async function _runPrompt() {
+  const answers = await inquirer.prompt(questions);
+  const {name, customStyle, customPalette} = answers;
+  const options: ICliOptions = {
+    themeName: name,
+    style: customStyle,
+    palette: customPalette,
+    outputPath: './'
+  }
+  return options;
+}
+
+// _buildConfig is used for both create default templates and generatin the theme
+// In the case that the "--all" flag is provide during the "new" command, the 
+// function will set the boolean. If not, then it grabs the value from args
+// which can either be a string, true, or undefined.
+function _buildConfig(args) {
+  const vsctbProvidesAll: boolean = args.all;
+  const options: ICliOptions = {
+    themeName: args.name,
+    style: (vsctbProvidesAll ? true : args.style),
+    palette: (vsctbProvidesAll ? true : args.palette),
+    outputPath: './'
+  }
+  return options;
 }
