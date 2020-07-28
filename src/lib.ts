78d9091dabf2d7ac71c9ab069da582111ab34525
SteVen Batten <steven.m.batten@outlook.com>
2020-07-27 19:39:21 -0700

    adding basics of generator

--- src/lib.ts
+++ src/lib.ts
@@ -1,11 +1,30 @@
-export function create(type: 'style' | 'palette' | 'all') {
+import * as fs from 'fs-extra';
+import * as path from 'path';
 
+
+export function create(type: 'style' | 'palette' | 'all') {
+  switch (type) {
+    case 'style':
+      fs.copySync(path.join(__dirname, 'templates/style'), path.join(process.cwd(), 'style/'));
+      break;
+    case 'palette':
+      fs.copySync(path.join(__dirname, 'templates/palette'), path.join(process.cwd(), 'palette/'));
+      break;
+    default:
+      fs.copySync(path.join(__dirname, 'templates/'), process.cwd(), { recursive: true });
+  }
 }
 
-export function build(palette, style) {
-
+export function build(palette, style, name) {
+  const theme = style.createTheme(palette, "light");
+  fs.writeFileSync(`vsctb-theme-${name}.json`,
+    JSON.stringify({
+      name,
+      colors: theme.colors
+    }, null, 2)
+  );
 }
 
 export function printUsage() {
-  
+
 }
