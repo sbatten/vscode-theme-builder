a38370b98e35350b0d00b9f7978a83bbc6e158a4
Tyler Townsend <townsend.ts@outlook.com>
2020-07-29 11:15:20 -0700

    updated name for output themes

--- src/lib.ts
+++ src/lib.ts
@@ -19,7 +19,7 @@ export function build(palette: any, style: any, name: string, outDir: string, va
   ['light', 'dark'].forEach(v => {
     if (variant === undefined || v === variant) {
       const theme = style.createTheme(palette, v);
-      const outputPath = path.join(outDir, `vsctb-theme-${name}-${v}.json`);
+      const outputPath = path.join(outDir, `${name}-${v}.json`);
       fs.ensureFileSync(outputPath);
       fs.writeJSONSync(outputPath,
         {
