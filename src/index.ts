ec9cff8cd885421fcfe0cb9dfad9a383729b4e8b
SteVen Batten <steven.m.batten@outlook.com>
2020-08-15 13:26:19 -0700

    fixes #1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+import * as path from 'path';
+import { addPath } from 'app-module-path';
+addPath(path.join(process.cwd(), "node_modules"));
+
 import { parse } from "./parser";
 
 parse();
